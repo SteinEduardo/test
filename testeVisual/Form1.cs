ceca1bafafd37265481d5d785f0a26215aae6035
Markin25 <marcospetrolini25@gmail.com>
2024-03-21 18:39:48 -0300

    Teste1.1
    Adicionei grandes coisas

--- testeVisual/Form1.cs
+++ testeVisual/Form1.cs
@@ -19,7 +19,7 @@ namespace testeVisual
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("pinto");
+            MessageBox.Show("PINTO GRANDE E FORTE");
         }
     }
 }
